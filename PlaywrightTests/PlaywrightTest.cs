f984f8805e711e2b0253b0adbe168e5b9c0d2b2c
SimonGriang <s.goettsberger@live.de>
2024-06-17 10:21:16 +0200

    deleted package references

--- PlaywrightTests/PlaywrightTest.cs
+++ PlaywrightTests/PlaywrightTest.cs
@@ -1,10 +1,17 @@
-namespace PlaywrightTests;
+using Microsoft.Playwright;
+using System.Threading.Tasks;
 
 [TestClass]
 public class PlaywrightTest
 {
     [TestMethod]
-    public void TestMethod1()
+    public async Task TestMethod1()
     {
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync();
+        var page = await browser.NewPageAsync();
+        await page.GoToAsync("http://www.example.com");
+        var title = await page.GetTitleAsync();
+        Assert.AreEqual("Example Domain", title);#
     }
 }
